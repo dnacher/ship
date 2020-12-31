4580b6b67e4cef7b414c739c9c58e2c51df0f571
DNacher <danielnacher@gmail.com>
2020-12-31 10:05:13 -0300

    se cambian los nombres viejos. Se agreganse crea carrito endpoint para hacer mas facil la administracion del carrito

--- src/main/java/com/ship/ship/domain/service/CartService.java
+++ src/main/java/com/ship/ship/domain/service/CartService.java
@@ -1,2 +1,29 @@
-package com.ship.ship.domain.service;public class CartService {
+package com.ship.ship.domain.service;
+
+import com.ship.ship.persistence.dao.CartDAO;
+import com.ship.ship.persistence.model.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
+public class CartService {
+
+    @Autowired
+    private CartDAO cartDAO;
+
+    public Cart getCartByHeaderId(Integer id){
+        return cartDAO.getCartByHeaderId(id);
+    }
+
+    public Cart saveCart(Cart cart){
+        return cartDAO.saveCart(cart);
+    }
+
+    public void deleteCart(Cart cart){
+        cartDAO.deleteCart(cart);
+    }
+
 }
